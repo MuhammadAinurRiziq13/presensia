478e4e8a568cc0d5b7e17fc47cd4428bba53240b
Muhammad Ainur Riziq <riziqainur13@gmail.com>
2024-10-09 08:33:36 +0700

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 // Import login.dart
-import 'splash_screen.dart'; // Import splash_screen.dart
-=======
-import 'pages/login/login.dart'; // Import login.dart
-import 'pages/splash_screen/splash_screen.dart'; // Import splash_screen.dart
->>>>>>> 773626dfa1f1dd7b4613bc9bdb4f39a3b977c834
+import 'package:presensia/pages/splash_screen/splash_screen.dart'; // Import splash_screen.dart
 
 void main() {
   runApp(const MyApp());
