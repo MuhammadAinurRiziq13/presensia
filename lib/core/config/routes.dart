c36d78b47b89cd644117761c1bfafc71510f7e36
Dayutirta <dayumahara502@gmail.com>
2024-12-03 23:28:03 +0700

    Default UI requestpermit

--- lib/core/config/routes.dart
+++ lib/core/config/routes.dart
@@ -190,7 +190,7 @@ class AppRoutes {
 
           return BlocProvider(
             create: (_) => PermitsBloc(permitUseCase),
-            child: const PermitRequestPage(),
+            child: PermitRequestPage(),
           );
         },
       ),
